51a2be9f0f8caeca84bd206a495ad41f96ad65d9
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2022-11-16 11:15:00 +0800

    Update dataframe_process.py

--- dataframe_process.py
+++ dataframe_process.py
@@ -1,5 +1,7 @@
 # Split strings into two Columns
-
 new = data["Name"].str.split(" ", n = 1, expand = True)
 data["First Name"]= new[0]
 data["Last Name"]= new[1]
+
+# Check if any value is NaN in a Pandas DataFrame
+df.isnull().any().any()
