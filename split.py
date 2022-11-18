73c54e62861253599c7984952b881327083fda4d
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2022-11-18 10:53:10 +0800

    Create split.py

---
+++ split.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/36300158/split-text-after-the-second-occurrence-of-character
+
+>>> a = "some-sample-filename-to-split"
+>>> "-".join(a.split("-", 2)[:2])
+'some-sample'
+
+# a.split("-", 2) will split the string upto the second occurrence of -.
+# a.split("-", 2)[:2] will give the first 2 elements in the list. Then simply join the first 2 elements.
