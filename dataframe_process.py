5f700b6467885a2bf127bfaad2963964fbce04b8
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2022-11-18 09:35:30 +0800

    Update dataframe_process.py

--- dataframe_process.py
+++ dataframe_process.py
@@ -5,3 +5,8 @@ data["Last Name"]= new[1]
 
 # Check if any value is NaN in a Pandas DataFrame
 df.isnull().any().any()
+
+# Create multiple dataframes in loop
+dfs = ['df1', 'df2', 'df3', 'df4']
+for df in dfs:
+     exec('{} = pd.DataFrame()'.format(df))
