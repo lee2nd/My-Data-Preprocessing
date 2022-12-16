58808fa2770949ecf41314a55e9950ba8166fb56
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2022-12-16 13:49:45 +0800

    Update dataframe_process.py

--- dataframe_process.py
+++ dataframe_process.py
@@ -63,7 +63,7 @@ df.drop(('a', 'c'), axis = 1)
 
 # Construct dataframe from dict
 data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
-pd.DataFrame.from_dict(data)
+df = pd.DataFrame.from_dict(data)
 
 #    col_1 col_2
 # 0      3     a
